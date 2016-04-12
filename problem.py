3534bb51eb79eb8862c39a1484c456b1c9f82cf4
rgoomes <ricardo.jpg@hotmail.com>
2016-04-12 12:51:20 +0100

    Added simple evolutionary algorithm with random population initialization, variation and repair operators, tournament selection to select parents and elitism selection to update the population

---
+++ problem.py
@@ -0,0 +1,174 @@
+from sys import *
+import random, os, operator, math
+
+if version_info.major == 3:
+	import configparser
+else:
+	import ConfigParser as configparser
+
+class Representation:
+	def get_weight(self, problem):
+		return sum([problem.item_weights[problem.items[i]]
+			for i in range(problem.total_items) if self.ks[i] == 1])
+
+	def get_ratio(self, item, problem):
+		return problem.item_values[problem.items[item]] \
+			/ problem.item_weights[problem.items[item]]
+
+	def repair(self, problem):
+		feno = [[i, problem.items[i], self.get_ratio(i, problem)]
+			for i in range(problem.total_items) if self.ks[i] == 1]
+
+		feno.sort(key=operator.itemgetter(2))
+		it, weight = 0, self.get_weight(problem)
+
+		while(weight > problem.W):
+			pos, item = feno[it][0], feno[it][1]
+			weight -= problem.item_weights[item]
+			self.ks[pos] = 0
+			it += 1
+
+	def isvalid(self, problem):
+		return self.get_weight(problem) <= problem.W
+
+	def evaluate(self, problem):
+		weight = value = time = 0.0
+
+		for i in range(problem.num_cities):
+			city = self.tsp[i]
+
+			for j in range(problem.bounds[city][0], problem.bounds[city][1]):
+				if self.ks[j] == 1:
+					item = problem.items[j]
+					value += problem.item_values[item]
+					weight += problem.item_weights[item]
+
+			vc = problem.vmax - weight * (problem.vmax - problem.vmin) / problem.W
+			time += problem.dists[city][self.tsp[(i+1) % problem.num_cities]] / vc
+
+		self.score = value - problem.R * time
+
+	def random_indiv(self, problem):
+		self.ks  = [random.randint(0,1) for _ in range(problem.total_items)]
+		self.tsp = random.sample(range(problem.num_cities), problem.num_cities)
+
+	def __init__(self, problem):
+		if problem != None:
+			self.random_indiv(problem);
+			self.repair(problem)
+
+class Problem:
+	def read_conf(self, conf):
+		if not os.path.isfile(conf):
+			print('ttp: invalid conf file')
+			exit(-1)
+
+		parser = configparser.ConfigParser()
+		parser.read(conf)
+
+		self.generations = int(parser.get('configs', 'generations'))
+		self.population_size = int(parser.get('configs', 'population_size'))
+		self.crossover_prob = float(parser.get('configs', 'crossover_prob'))
+		self.mutation_prob = float(parser.get('configs', 'mutation_prob'))
+		self.elite_size = float(parser.get('configs', 'elite_size'))
+		self.tournament_size = int(parser.get('configs', 'tournament_size'))
+
+	def read_input1(self):
+		self.num_cities = int(stdin.readline().rstrip())
+		num_items  = int(stdin.readline().rstrip())
+		self.W = float(stdin.readline().rstrip())
+		self.vmax = float(stdin.readline().rstrip())
+		self.vmin = float(stdin.readline().rstrip())
+		self.R = float(stdin.readline().rstrip())
+
+		self.dists = []
+		for i in range(self.num_cities):
+			distances = [float(n) for n in stdin.readline().rstrip().split()]
+			self.dists.append(distances)
+
+		self.item_weights = [float(n) for n in stdin.readline().rstrip().split()]
+		self.item_values = [float(n) for n in stdin.readline().rstrip().split()]
+
+		item_locations = []
+		for i in range(num_items):
+			locations = [int(n) for n in stdin.readline().rstrip().split()]
+			item_locations.append(locations)
+
+		self.total_items = sum(map(sum, item_locations))
+		self.items = []
+		self.bounds = []
+
+		total = 0
+		for i in range(self.num_cities):
+			self.bounds.append([total, 0])
+			for j in range(num_items):
+				if item_locations[j][i] == 1:
+					total += 1
+					self.bounds[i][1] = total
+					self.items.append(j)
+
+	def read_input2(self):
+		stdin.readline(), stdin.readline()
+
+		self.num_cities = int(stdin.readline().split('\t')[1])
+		self.total_items = int(stdin.readline().split('\t')[1])
+		self.W = float(stdin.readline().split('\t')[1])
+		self.vmin = float(stdin.readline().split('\t')[1])
+		self.vmax = float(stdin.readline().split('\t')[1])
+		self.R = float(stdin.readline().split('\t')[1])
+
+		stdin.readline(), stdin.readline()
+		cities = []
+
+		for i in range(self.num_cities):
+			tmp = stdin.readline().split()
+			cities.append([int(tmp[0]), int(tmp[1])])
+
+		self.dists = [[0.0 for _ in range(self.num_cities)] for _ in range(self.num_cities)]
+		for i in range(self.num_cities):
+			for j in range(self.num_cities):
+				if i != j:
+					self.dists[i][j] = math.sqrt((cities[i][0] - cities[j][0])**2
+						+ (cities[i][1] - cities[j][1])**2)
+
+		stdin.readline()
+		itemset = set()
+		all_items = []
+		self.bounds = [[0, 0] for _ in range(self.num_cities)]
+
+		for i in range(self.total_items):
+			tmp = stdin.readline().split()
+			all_items.append([(float(tmp[1]), float(tmp[2])), int(tmp[3])])
+			itemset.add((float(tmp[1]), float(tmp[2])))
+			self.bounds[int(tmp[3])-1][1] += 1
+
+		itemset = list(itemset)
+
+		self.item_weights = [w for (v,w) in itemset]
+		self.item_values = [v for (v,w) in itemset]
+
+		for i in range(1, self.num_cities):
+			self.bounds[i][0] = self.bounds[i-1][1]
+			self.bounds[i][1] += self.bounds[i-1][1]
+
+		self.items = []
+		for i in range(self.num_cities):
+			for j in range(self.total_items):
+				if(all_items[j][1] - 1 == i):
+					self.items.append(itemset.index(all_items[j][0]))
+
+	def __init__(self, args):
+		self._nodes = 0
+		self.best_cromo = None
+		self.best_score = -float("inf")
+
+		self.debug = args[0]
+		self.read_conf(args[1])
+
+		if int(args[2]) == 1:
+			self.read_input1()
+		elif int(args[2]) == 2:
+			self.read_input2()
+		else:
+			print('ttp: invalid test case type')
+			exit(-1)
---
+++ ttp.py
@@ -0,0 +1,159 @@
+import cProfile
+
+from sys import *
+from problem import *
+from random import *
+
+def gen_population(problem):
+	return [Representation(problem) for _ in range(problem.population_size)]
+
+def eval_population(population, problem):
+	for i in range(problem.population_size):
+		population[i].evaluate(problem)
+
+# tournament selection
+def select_parents(population, problem):
+	parents = [Representation(None) for _ in range(problem.population_size)]
+
+	for i in range(problem.population_size):
+		points = sample(range(problem.population_size), problem.tournament_size)
+		subset = [[population[pos].score, pos] for pos in points]
+		pos = max(subset, key=lambda p: p[0])[1]
+
+		parents[i].ks, parents[i].tsp = population[pos].ks[:], population[pos].tsp[:]
+
+	return parents
+
+# knapsack crossver
+def two_point(cromo1, cromo2, problem):
+	if random() < problem.crossover_prob:
+		return
+
+	points = sample(range(problem.total_items), 2)
+	pos1, pos2 = min(points), max(points)
+
+	child1 = cromo1.ks[:pos1] + cromo2.ks[pos1:pos2] + cromo1.ks[pos2:]
+	child2 = cromo2.ks[:pos1] + cromo1.ks[pos1:pos2] + cromo2.ks[pos2:]
+	cromo1.ks, cromo2.ks = child1, child2
+
+# tsp crossver
+def pmx(cromo1, cromo2, problem):
+	if random() < problem.crossover_prob:
+		return
+
+	points = sample(range(problem.num_cities), 2)
+	beg, end = min(points), max(points)
+
+	child1, child2 = cromo1.tsp[:], cromo2.tsp[:]
+	child1[beg:end+1] = cromo2.tsp[beg:end+1]
+	child2[beg:end+1] = cromo1.tsp[beg:end+1]
+
+	for parent, child in zip([cromo1.tsp, cromo2.tsp], [child1, child2]):
+		for i in range(beg, end+1):
+			if parent[i] not in child[beg:end+1]:
+				spot = i
+				while beg <= spot <= end:
+					spot = parent.index(child[spot])
+				child[spot] = parent[i]
+
+	cromo1.tsp, cromo2.tsp = child1, child2
+
+def apply_mutation(cromo, problem):
+	# knapsack - xor mutation
+	for i in range(problem.total_items):
+		if random() < problem.mutation_prob:
+			cromo.ks[i] ^= 1
+
+	# tsp - swap mutation
+	if random() < problem.mutation_prob:
+		pos1, pos2 = sample(range(problem.num_cities), 2)
+		cromo.tsp[pos1], cromo.tsp[pos2] = cromo.tsp[pos2], cromo.tsp[pos1]
+
+def apply_variation(parents, problem):
+	for i in range(0, problem.population_size, 2):
+		two_point(parents[i], parents[i+1], problem)
+		pmx(parents[i], parents[i+1], problem)
+
+	for i in range(problem.population_size):
+		apply_mutation(parents[i], problem)
+		parents[i].repair(problem)
+
+	return parents
+
+def select_survivors(population, offsprings, problem):
+	population.sort(key=lambda i: i.score, reverse=True)
+	offsprings.sort(key=lambda i: i.score, reverse=True)
+
+	comp = int(problem.population_size * problem.elite_size)
+	return population[:comp] + offsprings[:problem.population_size - comp]
+
+def update_fittest(population, problem):
+	new_best = False
+
+	for i in range(problem.population_size):
+		if not population[i].isvalid(problem):
+			problem._nodes += 1
+		elif population[i].score > problem.best_score:
+			problem.best_score = population[i].score
+			problem.best_cromo = [population[i].tsp[:], population[i].ks[:]]
+			new_best = True
+
+	return new_best
+
+def print_status(generation, problem, new_best):
+	if not problem.debug:
+		return
+	if generation > 1:
+		stdout.write("\r" if not new_best else "\n")
+
+	stdout.write("info generation " + str(generation) + " ")
+	stdout.write("bestscore " + str(problem.best_score) + " ")
+	stdout.write("invalid nodes " + str(problem._nodes))
+	stdout.write("\n" if generation == problem.generations else "")
+	stdout.flush()
+
+def sea(problem):
+	population = gen_population(problem)
+	eval_population(population, problem)
+
+	for i in range(problem.generations):
+		parents = select_parents(population, problem)
+		offsprings = apply_variation(parents, problem)
+
+		eval_population(offsprings, problem)
+		population = select_survivors(population, offsprings, problem)
+		new_best = update_fittest(population, problem)
+
+		print_status(i+1, problem, new_best)
+
+	print("info bestscore " + str(problem.best_score) + ("\n" if '--prof' in argv else ""))
+
+def parse_args():
+	if '--help' in argv:
+		print('usage: ttp [OPTION...] [FILE...]')
+		print('  --help    display this help and exit')
+		print('  --prof    enable profiling')
+		print('  --debug   enable debugging')
+		print('  --conf F  use F as a config file (mandatory)')
+		print('  --type T  use T as the type of the test case (mandatory)')
+		print('            valid test case types are 1 and 2\n')
+	elif '--conf' not in argv or argv.index('--conf')+1 == len(argv):
+		print('ttp: missing or invalid option --conf')
+		print('Try \'ttp --help\' for more information.\n')
+	elif '--type' not in argv or argv.index('--type')+1 == len(argv):
+		print('ttp: missing or invalid option --type')
+		print('Try \'ttp --help\' for more information.\n')
+	else:
+		return ['--debug' in argv, argv[argv.index('--conf')+1], argv[argv.index('--type')+1]]
+
+	exit(-1)
+
+def main():
+	problem = Problem(parse_args())
+
+	if '--prof' in argv:
+		cProfile.runctx('sea(problem)', {'sea':sea}, {'problem':problem}, sort=1)
+	else:
+		sea(problem)
+
+main()
